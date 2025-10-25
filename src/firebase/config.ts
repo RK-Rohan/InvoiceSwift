530619f2e13f8fc1b24420347991747682a98dfc
Rezaul Karim <rkrohan94@gmail.com>
2025-10-25 15:13:31 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // Firebase configuration will be handled by the backend.
 // In a real-world scenario, you would have your Firebase config here.
 export const firebaseConfig = {
-  apiKey: "AIzaSyDPG19P7sAnw_8yHjYDB-9Vf_2t3hfWdDA",
-  authDomain: "prompt-lab-329114.firebaseapp.com",
-  projectId: "prompt-lab-329114",
-  storageBucket: "prompt-lab-329114.appspot.com",
-  messagingSenderId: "354572793134",
-  appId: "1:354572793134:web:0b25e1a77ba17a0a7f1a3a",
-  measurementId: "G-C2S39Z2Y1J"
+  "projectId": "studio-4060005145-2f660",
+  "appId": "1:75709684283:web:6e8c478ff171570af89a1e",
+  "apiKey": "AIzaSyBuvJsnUH2qba9Fx61I1Wf2N0eCU-hGUXI",
+  "authDomain": "studio-4060005145-2f660.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "75709684283",
+  "storageBucket": "studio-4060005145-2f660.appspot.com"
 };
