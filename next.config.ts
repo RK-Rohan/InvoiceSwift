6184ff0e83e81bcf9b5aa510fb7b4c0c3c57dda7
Rezaul Karim <rkrohan94@gmail.com>
2025-10-27 16:10:14 +0000

    Request URL https://6000-firebase-studio-1761403003838.cluster-bg6uurscp

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    originalStackFrame: false,
+  },
   images: {
     remotePatterns: [
       {
